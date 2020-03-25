a5eb5064dba5c207fa3438982f55bd512ba605a8
dcep93 <dan@quizlet.com>
2020-03-24 18:11:55 -0700

    fixes

--- app/public/index.js
+++ app/public/index.js
@@ -7,9 +7,9 @@
 // state.preplace bool
 
 $(document).ready(function() {
-	$('#start_over').click(prepare);
-	$('#bid_form').submit(bid);
-	$('#pass').click(pass);
+	$("#start_over").click(prepare);
+	$("#bid_form").submit(bid);
+	$("#pass").click(pass);
 });
 
 var oldAdvanceTurn = advanceTurn;
@@ -34,19 +34,19 @@ function needsToPreplace(player) {
 
 function bid() {
 	setTimeout(function() {
-		if (!isMyTurn()) return alert('not your turn');
-		var bidAmount = Number.parseInt($('#bid_input').val());
-		if (isNaN(bidAmount) || bidAmount <= 0) return alert('invalid bid');
+		if (!isMyTurn()) return alert("not your turn");
+		var bidAmount = Number.parseInt($("#bid_input").val());
+		if (isNaN(bidAmount) || bidAmount <= 0) return alert("invalid bid");
 		var maxBid = 0;
 		for (var i = 0; i < state.players.length; i++) {
 			maxBid += state.players[i].state.played.length;
 		}
-		if (bidAmount > maxBid) return alert('bid too high');
+		if (bidAmount > maxBid) return alert("bid too high");
 		state.bid = bidAmount;
 		state.bidPlayer = myIndex;
-		var message = 'bid ' + bidAmount;
-		if (bid === maxBid || allPassed()) {
-			message += ' - starting flip';
+		var message = "bid " + bidAmount;
+		if (bidAmount === maxBid || allPassed()) {
+			message += " - starting flip";
 			state.numFlipped = 0;
 		} else {
 			advanceTurn();
@@ -59,12 +59,12 @@ function bid() {
 function pass() {
 	if (isMyTurn() && state.bidPlayer !== null) {
 		me().state.passed = true;
-		var message = 'passed';
+		var message = "passed";
 		if (allPassed()) {
 			message +=
-				' - [' +
+				" - [" +
 				state.players[state.bidPlayer].name +
-				'] starting flip';
+				"] starting flip";
 			state.currentPlayer = state.bidPlayer;
 			state.numFlipped = 0;
 		} else {
@@ -72,36 +72,37 @@ function pass() {
 		}
 		sendState(message);
 	}
+	return false;
 }
 
 function flip() {
-	if (!isMyTurn() || state.numFlipped === null) return alert('cant flip');
-	var index = $(this).attr('data-index');
+	if (!isMyTurn() || state.numFlipped === null) return alert("cant flip");
+	var index = $(this).attr("data-index");
 	if (index !== myIndex && me().state.played.length !== 0)
-		return alert('need to flip own tiles first');
+		return alert("need to flip own tiles first");
 	var player = state.players[index];
 	var playerState = player.state;
 	if (playerState.played.length === 0)
-		return alert('player has no played tiles');
+		return alert("player has no played tiles");
 	var revealed = playerState.played.pop();
 	playerState.revealed.push(revealed);
-	var message = 'flipped - [' + player.name + '] - ';
+	var message = "flipped - [" + player.name + "] - ";
 	if (revealed) {
-		message += 'FLOWER';
+		message += "FLOWER";
 		if (++state.numFlipped === state.bid) {
 			me().state.points++;
-			message += ' - point awarded';
+			message += " - point awarded";
 			finishRound();
 		}
 	} else {
-		message += 'SKULL';
+		message += "SKULL";
 		finishRound();
 		var myState = me().state;
 		if (myState.hand.length === 1 || index !== myIndex) {
 			shuffleArray(myState.hand);
 			myState.hand.pop();
 			myState.hand.sort();
-			message += ' - and loses a card';
+			message += " - and loses a card";
 		} else {
 			state.numFlipped = -1;
 		}
@@ -128,12 +129,12 @@ function allPassed() {
 
 function play() {
 	if (isMyTurn() && state.bidPlayer === null) {
-		var index = $(this).attr('data-index');
+		var index = $(this).attr("data-index");
 		me().state.played.push(me().state.hand.splice(index, 1)[0]);
-		var message = 'placed a tile';
+		var message = "placed a tile";
 		if (state.preplace) {
 			if (allHavePreplaced()) {
-				message += ' - starting round';
+				message += " - starting round";
 				state.preplace = false;
 			}
 		} else {
@@ -152,13 +153,13 @@ function allHavePreplaced() {
 
 function prepare() {
 	state.currentPlayer = myIndex;
-	state.numFlowers = Number.parseInt($('#num_flowers').val());
-	state.numSkulls = Number.parseInt($('#num_skulls').val());
+	state.numFlowers = Number.parseInt($("#num_flowers").val());
+	state.numSkulls = Number.parseInt($("#num_skulls").val());
 	for (var i = 0; i < state.players.length; i++) {
 		state.players[i].state = newState();
 	}
 	resetBid();
-	sendState('prepare');
+	sendState("prepare");
 }
 
 function newState() {
@@ -166,7 +167,7 @@ function newState() {
 		points: 0,
 		played: [],
 		hand: [],
-		revealed: [],
+		revealed: []
 	};
 	for (var i = 0; i < state.numFlowers; i++) {
 		playerState.hand.push(true);
@@ -191,65 +192,65 @@ function resetBid() {
 
 function penalty() {
 	if (!isMyTurn() || state.numFlipped !== -1)
-		return alert('cannot pay penalty');
+		return alert("cannot pay penalty");
 	x = this;
-	var index = $(this).attr('data-index');
+	var index = $(this).attr("data-index");
 	me().state.hand.splice(index, 1);
 	state.numFlipped = null;
-	sendState('discarded a tile');
+	sendState("discarded a tile");
 }
 
 function update() {
-	$('#num_flowers').val(state.numFlowers);
-	$('#num_skulls').val(state.numSkulls);
-	$('#hand').empty();
+	$("#num_flowers").val(state.numFlowers);
+	$("#num_skulls").val(state.numSkulls);
+	$("#hand").empty();
 	for (var i = 0; i < me().state.hand.length; i++) {
 		var tile = me().state.hand[i];
-		$('<div>')
-			.attr('data-index', i)
-			.text(tile ? 'FLOWER' : 'SKULL')
-			.addClass('tile')
-			.addClass('bubble')
-			.addClass('inline')
-			.appendTo('#hand');
+		$("<div>")
+			.attr("data-index", i)
+			.text(tile ? "FLOWER" : "SKULL")
+			.addClass("tile")
+			.addClass("bubble")
+			.addClass("inline")
+			.appendTo("#hand");
 	}
-	$('#players_state').empty();
+	$("#players_state").empty();
 	for (var i = 0; i < state.players.length; i++) {
 		var player = state.players[i];
-		$('<div>')
-			.attr('data-index', i)
-			.addClass('player_state')
-			.addClass('bubble')
-			.addClass('inline')
-			.append($('<p>').text(player.name))
-			.append($('<p>').text('points: ' + player.state.points))
-			.append($('<p>').text('hand: ' + player.state.hand.length))
-			.append($('<p>').text('played: ' + player.state.played.length))
-			.append($('<p>').text('revealed: ' + player.state.revealed.length))
-			.appendTo('#players_state');
+		$("<div>")
+			.attr("data-index", i)
+			.addClass("player_state")
+			.addClass("bubble")
+			.addClass("inline")
+			.append($("<p>").text(player.name))
+			.append($("<p>").text("points: " + player.state.points))
+			.append($("<p>").text("hand: " + player.state.hand.length))
+			.append($("<p>").text("played: " + player.state.played.length))
+			.append($("<p>").text("revealed: " + player.state.revealed.length))
+			.appendTo("#players_state");
 	}
 	if (isMyTurn() && state.numFlipped === null) {
 		var playingFirstTile = state.preplace && me().state.played.length === 0;
-		$('.bid_control').prop('disabled', playingFirstTile);
+		$(".bid_control").prop("disabled", playingFirstTile);
 		if (state.bidPlayer === null) {
-			$('.tile')
-				.addClass('hover_pointer')
+			$(".tile")
+				.addClass("hover_pointer")
 				.click(play);
-			$('#pass').prop('disabled', true);
+			$("#pass").prop("disabled", true);
 		}
 	} else {
-		$('.bid_control').prop('disabled', true);
+		$(".bid_control").prop("disabled", true);
 		if (isMyTurn()) {
 			if (state.numFlipped === -1) {
-				$('.tile')
-					.addClass('hover_pointer')
+				$(".tile")
+					.addClass("hover_pointer")
 					.click(penalty);
 				setTimeout(function() {
-					alert('select a tile to discard');
+					alert("select a tile to discard");
 				});
 			} else {
-				$('.player_state')
-					.addClass('hover_pointer')
+				$(".player_state")
+					.addClass("hover_pointer")
 					.click(flip);
 			}
 		}
